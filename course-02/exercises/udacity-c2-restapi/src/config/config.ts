26146862a217ec685a67580175dc21320aacfa29
Adewole Adekunle <adewoleadekunle23@yahoo.com>
2022-06-09 22:13:36 +0100

    added database cred

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "kunledatabase",
+    "password": "unstoppable232",
+    "database": "database-1",
+    "host": "database-1.ctsnbr3okvu3.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "kunleadebuckdev"
   },
   "jwt": {
     "secret": " "
